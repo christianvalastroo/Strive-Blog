415c87f1d40010823998822d07a0d8936b0bbf40
Christian Valastro <christianvalastro@icloud.com>
2026-06-03 22:28:31 +0200

    Aggiunta endpoint POST autori

--- Backend/routes/authors.js
+++ Backend/routes/authors.js
@@ -3,14 +3,42 @@ const Author = require("../models/Author")
 
 const authorsRouter = express.Router()
 
+// GET tutti gli autori
 authorsRouter.get("/", async (req, res) => {
     try {
         const authors = await Author.find()
 
-        res.status(200).send(authors)
+        res.status(200).json({
+            statusCode: 200,
+            message: "OK",
+            data: authors
+        })
+
     } catch (error) {
-        res.status(500).send({
-            message: "Errore nel recupero degli autori"
+        res.status(500).json({
+            statusCode: 500,
+            message: "Internal Server Error"
+        })
+    }
+})
+
+// POST crea autore
+authorsRouter.post("/", async (req, res) => {
+    try {
+        const newAuthor = new Author(req.body)
+
+        const savedAuthor = await newAuthor.save()
+
+        res.status(201).json({
+            statusCode: 201,
+            message: "Created",
+            data: savedAuthor
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            statusCode: 400,
+            message: "Bad Request"
         })
     }
 })
