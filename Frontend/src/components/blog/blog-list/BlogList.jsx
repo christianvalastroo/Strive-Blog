f56c274a94bb111e24881d8b1c9d795bad9ad960
Christian Valastro <christianvalastro@icloud.com>
2026-06-03 23:29:42 +0200

    Collegamento frontend ai blog post

--- Frontend/src/components/blog/blog-list/BlogList.jsx
+++ Frontend/src/components/blog/blog-list/BlogList.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = props => {
+  const [posts, setPosts] = useState([]);
+
+  const getBlogPosts = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/blogPosts");
+      const result = await response.json();
+
+      setPosts(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBlogPosts();
+  }, []);
+
   return (
     <Row>
       {posts.map((post, i) => (
         <Col
-          key={`item-${i}`}
+          key={post._id || `item-${i}`}
           md={4}
           style={{
             marginBottom: 50,
           }}
         >
-          <BlogItem key={post.title} {...post} />
+          <BlogItem {...post} />
         </Col>
       ))}
     </Row>
